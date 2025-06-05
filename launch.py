a494da60bbe92557a128c163354bb55da37fae3b
Finley Jones <finley@finleyjones.co.uk>
2025-06-05 14:04:56 +0100

    delayed launch until explorer.exe

--- launch.py
+++ launch.py
@@ -1,4 +1,12 @@
 import subprocess, time
+import psutil
+
+def wait_for_explorer():
+    while not any(p.name().lower() == "explorer.exe" for p in psutil.process_iter()):
+        time.sleep(1)
+
+wait_for_explorer()
+
 
 # Run the updater
 subprocess.run(["updater.exe"])
